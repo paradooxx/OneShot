b96ba49d74e72c02bbf7dfce8bba8d9cffad84c2
theParas <parassth63@gmail.com>
2024-06-10 08:01:08 +0545

    main menu management

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject levelPanel;
 
+    [SerializeField] private Button buttonStart,
+                                    buttonLevelSelect,
+                                    buttonLevelMenuExit;
+
     private void Awake()
     {
         levelPanel.SetActive(false);
+    }
+
+    private void Start()
+    {
+        buttonStart?.onClick.AddListener(() => LevelManager.instance.StartLevel());
+        buttonLevelSelect?.onClick.AddListener(() => ActivateLevelPanel());
+        buttonLevelMenuExit?.onClick.AddListener(() => LevelMenuExitClick());
     }
 
     public void LevelMenuExitClick()
